5cae64659534f984ead2e1cf5356291d1429fd63
HT_NDUJMOVIC <Neven.Dujmovic@t.ht.hr>
2020-11-10 16:49:03 +0100

    first commit

---
+++ src/test/java/org/acme/NativeEquationSolverIT.java
@@ -0,0 +1,9 @@
+package org.acme;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeEquationSolverIT extends EquationSolverTest {
+
+    // Execute the same tests but in native mode.
+}
